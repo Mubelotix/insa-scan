02a8359148188ac9a49f98586b871639af816423
Mubelotix <mubelotix@gmail.com>
2024-01-21 11:00:44 +0100

    Animate on scroll

--- site/script.js
+++ site/script.js
@@ -52,3 +52,18 @@ var chart = new Chart(
     document.getElementById('summary-chart-canvas'),
     config
 );
+
+// Scroll animations
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            entry.target.classList.remove('hidden');
+        } //else {
+        //    entry.target.classList.remove('show');
+        //    entry.target.classList.add('hidden');
+        //}
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
